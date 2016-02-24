656121a23dd1050227b114621398706faa555dd2
PStar <shlin65@me.com>
2016-02-24 17:34:59 +0800

    新增MSSQL全形轉半形
    將此 function 安裝到目標的資料庫，就可以使用。
    
    目標是將英文、數字、符號的全形文字轉換為半形
    
    使用方法以 update 方式進行 ex：
    
    UPDATE [資料表名稱] SET [資料欄位名稱] = dbo.f_Convert([資料欄位名稱], 0);

---
+++ MSSQL.sql
@@ -0,0 +1,41 @@
+CREATE FUNCTION f_Convert( 
+
+@str NVARCHAR(4000), --要轉換的字元串 
+
+@flag bit --轉換標誌,0轉換成半形,1轉換成全形 
+
+)RETURNS nvarchar(4000) 
+
+AS 
+
+BEGIN 
+
+DECLARE @pat nvarchar(8),@step int,@i int,@spc int 
+
+IF @flag=0 
+
+SELECT @pat=N'%[！-～]%',@step=-65248, 
+
+@str=REPLACE(@str,N'　 ',N' ') 
+
+ELSE 
+
+SELECT @pat=N'%[!-~]%',@step=65248, 
+
+@str=REPLACE(@str,N' ',N'　 ') 
+
+SET @i=PATINDEX(@pat COLLATE LATIN1_GENERAL_BIN,@str) 
+
+WHILE @i> 0 
+
+SELECT @str=REPLACE(@str, 
+
+SUBSTRING(@str,@i,1), 
+
+NCHAR(UNICODE(SUBSTRING(@str,@i,1))+@step)) 
+
+,@i=PATINDEX(@pat COLLATE LATIN1_GENERAL_BIN,@str) 
+
+RETURN(@str) 
+
+END 
